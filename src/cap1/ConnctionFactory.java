41903a0a839219e1f74c6fec14693adcb49f81fb
Claudio Costa <clacosta@gmail.com>
2019-11-07 16:40:51 -0300

    feet: 01 - 04 - Melhorando a fábrica

--- src/cap1/ConnctionFactory.java
+++ src/cap1/ConnctionFactory.java
@@ -5,12 +5,39 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnctionFactory {
+	
+	private String tipoBanco = System.getenv("tipoBanco");
+	
+	public ConnctionFactory() {
+		
+		this.tipoBanco = retornaTipoDoBanco();
+		
+	}
+	
+
+	private String retornaTipoDoBanco() {
+		
+		String variavelAmbienteTipoBanco  = retornaVariavelAmbienteTipoBanco();
+		
+		if(variavelAmbienteTipoBanco.equals("mysql")) {
+			return "mysql";
+		}
+		
+		return "postgres";
+		
+	}
+
+	private String retornaVariavelAmbienteTipoBanco() {
+		
+		return System.getenv("tipoBanco");
+		
+	}
 
 	public Connection getConnection() {
 		
 		try {
 		
-			Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/banco", "root", "123");
+			Connection c = DriverManager.getConnection("jdbc:" + this.tipoBanco + "://localhost:3306/banco", "root", "123");
 			return c;
 		
 		} catch (SQLException e) {
