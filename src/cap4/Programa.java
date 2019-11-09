129b067782f6e5025302faadaec49862bf276f18
Claudio Costa <clacosta@gmail.com>
2019-11-09 19:09:44 -0300

    feet: 04 - 05 - Novas expressões

--- src/cap4/Programa.java
+++ src/cap4/Programa.java
@@ -6,7 +6,7 @@ public class Programa {
 		
 		Expressao esquerda = new Subtracao(new Soma(new Numero(5), new Numero(7)), new Numero(5));
 		Expressao direita = new Soma(new Numero(2), new Numero(10));
-		Expressao soma = new Soma(esquerda, direita);
+		Expressao soma = new Soma(new Multiplicacao(new Divisao(new Numero(10), new Numero(5)), new Numero(2)), direita);
 		
 		int resultado = soma.avalia();
 		
